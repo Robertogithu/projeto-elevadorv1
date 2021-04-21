8380caa6ddb3b0b3445a64fe78cb23ac3888bdf0
Roberto Alves Martins <>
2021-04-21 01:11:52 +0000

    gettotalmove

--- Elevador.java
+++ Elevador.java
@@ -4,38 +4,56 @@ class Elevador{
   private int estado; //0,1,2, ou 3
   private int posicao; 
   private int totalrun; //historico de deslocamento
-
+  private int totalmove;//historico de movimentos
 
  public void inicializar(String codigo){
    if(this.estado == 0){
     this.codigo = codigo;
     this.posicao = 0;
     this.totalrun = 0;
+    this.estado = 1; //disponivel para uso
    } else{
      System.out.println("(debug) falha!");
    }
  }
  
  public void ligar(){
-    if(this.estado == 0)
+    if(this.estado == 0){
       this.estado = 1;
-    else{
+    }else{
      System.out.println("(debug) falha operação ilegal !");
    }
 }
 
  public void desligar(){
-if(this.estado == 1 && this.posicao == 0)
+if(this.estado == 1 && this.posicao == 0){
       this.estado = 0;
-    else{
+    }else{
      System.out.println("(debug) falha operação ilegal !");
    }
  }
 
  public void mover(int destino){
     
-    if(this.estado == 1 && destino > 0){
-    this.posicao = destino;
+    if(this.estado == 1 && destino >= 0){
+         
+       int delta;
+
+       if(destino > this.posicao){
+          System.out.println("\n(debug) subindo.." + destino);
+
+          delta = destino - posicao;
+       }else{
+         System.out.println("\n(debug) descendo..." + destino);
+
+         delta = posicao - destino;
+       }
+      
+
+      totalrun = totalrun + delta;
+      
+      this.posicao = destino;
+
     } else {
       System.out.println("(debug) falha operação ilegal !");
     }
@@ -43,14 +61,18 @@ if(this.estado == 1 && this.posicao == 0)
 
  }
 
- public int getPosicao(int novaposicao){
-   return this.posicao + novaposicao;
+ public int getPosicao(){
+   return this.posicao;
  }
  public int getTotalRun(){
    return this.totalrun;
  }
  public String getCodigo(){
    return this.codigo;
+ }
+
+ public int getTotalMove(){
+        return this.totalmove;
  }
  
 
--- Main.java
+++ Main.java
@@ -1,6 +1,40 @@
 class Main {
   public static void main(String[] args) 
   {
+    
+    Elevador elevador = new Elevador();
+
+
+    elevador.inicializar("TORRE01");
+
+
+    System.out.println("NOME: " + elevador.getCodigo());
+    System.out.println("ANDAR -->" + elevador.getPosicao());
+
+    elevador.mover(7);
+    System.out.println("\nnome: "+ elevador.getCodigo() + "["+ elevador.getTotalRun() +"]");
+    System.out.println("parado no andar -->" + elevador.getPosicao());
+                                                 
+    elevador.mover(12);
+    System.out.println("\nnome: "+ elevador.getCodigo() + "["+ elevador.getTotalRun() +"]");
+    System.out.println("parado no andar -->" + elevador.getPosicao());
+    
+    elevador.mover(3);
+    System.out.println("\nnome: "+ elevador.getCodigo() + "["+ elevador.getTotalRun() +"]");
+    System.out.println("parado no andar -->" + elevador.getPosicao());
+    
+    elevador.mover(0);
+    System.out.println("\nnome: "+ elevador.getCodigo() + "["+ elevador.getTotalRun() +"]");
+    System.out.println("parado no andar -->" + elevador.getPosicao());
+    
+    elevador.desligar();
+    elevador.inicializar("TOWER EST");
+
+    
+    System.out.println("\nnome: "+ elevador.getCodigo() + "["+ elevador.getTotalRun() +"]");
+    System.out.println("Parado no andar --> " + elevador.getPosicao());
+    
+
     System.out.println("fim de teste");
 
     
